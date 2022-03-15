1924e9ea3e4160999082c7bd659d6c16bc10d710
Murtaza Pakawala <mypaka52@gmail.com>
2022-03-15 18:49:20 +1100

    the api  is working fine and the image is loading with the glide library into ivMeme

--- app/src/main/java/com/example/memegenerator/MainActivity.kt
+++ app/src/main/java/com/example/memegenerator/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.memegenerator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.Log
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
+private const val TAG ="M"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+        loadMeme()
+
     }
-}
+
+    private fun loadMeme() {
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://meme-api.herokuapp.com/gimme"
+
+// Request a string response from the provided URL.
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+            Response.Listener { response ->
+                    val url = response.getString("url")
+                Glide.with(this).load(url).into(ivMeme)
+            },
+            Response.ErrorListener { error ->
+                Log.d(TAG,"error is ${error}")
+            }
+        )
+
+// Add the request to the RequestQueue.
+        queue.add(jsonObjectRequest)
+    }
+
+    }
