0c4938ba9dd0e4ac6a9505b80d058133edb86399
Murtaza Pakawala <mypaka52@gmail.com>
2022-03-16 12:40:20 +1100

    share button working have implemented the intent ACTION_SEND

--- app/src/main/java/com/example/memegenerator/MainActivity.kt
+++ app/src/main/java/com/example/memegenerator/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.memegenerator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -13,20 +14,35 @@ import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 private const val TAG ="M"
 class MainActivity : AppCompatActivity() {
+    var currUrl: String? =null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
         loadMeme()
+        btnNext.setOnClickListener{
+            loadMeme()
+        }
+        btnShare.setOnClickListener{
+            share()
+        }
+
+    }
+    fun share(){
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type ="text/plain"
+        intent.putExtra(Intent.EXTRA_TEXT,"See this is so Funny!!!! ${currUrl}")
+        val chooser = Intent.createChooser(intent,"share this meme")
+        startActivity(chooser)
 
     }
 
     private fun loadMeme() {
         val queue = Volley.newRequestQueue(this)
-        val url = "https://meme-api.herokuapp.com/gimme"
+        currUrl = "https://meme-api.herokuapp.com/gimme"
 
 // Request a string response from the provided URL.
-        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, currUrl, null,
             Response.Listener { response ->
                     val url = response.getString("url")
                 Glide.with(this).load(url).into(ivMeme)
@@ -39,5 +55,6 @@ class MainActivity : AppCompatActivity() {
 // Add the request to the RequestQueue.
         queue.add(jsonObjectRequest)
     }
+
 
     }
